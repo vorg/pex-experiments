b317a5f1eb0ee5200da0a8d192b8067a1940e393
Marcin Ignac <marcin.ignac@gmail.com>
2016-03-16 23:57:00 +0000

    oit added fbo and switched to entities

--- oit/main.js
+++ oit/main.js
@@ -11,13 +11,16 @@ var normals         = require('angle-normals'); //https://www.npmjs.com/package/
 var rescaleVertices = require('rescale-vertices');
 var AABB            = require('pex-geom/AABB');
 var Vec3            = require('pex-math/Vec3');
-var computeSmoothNormals = require('./local_modules/geom-compute-smooth-normals')
+var Mat4            = require('pex-math/Mat4');
+var computeSmoothNormals = require('./local_modules/geom-compute-smooth-normals');
+var createFx        = require('pex-fx');
+var GUI             = require('pex-gui');
 
 Window.create({
     settings: {
         width:  1280,
         height: 720,
-        fullScreen: isBrowser ? true : true
+        fullScreen: isBrowser ? true : false
     },
     resources: {
         showNormalsVert: { glsl: glslify(__dirname + '/assets/ShowNormals.vert') },
@@ -27,7 +30,22 @@ Window.create({
         var ctx = this.getContext();
         var res = this.getResources();
 
-        this.camera  = new PerspCamera(45,this.getAspectRatio(),0.001,20.0);
+        var w = this.getWidth();
+        var h = this.getHeight();
+
+        var gui = this.gui = new GUI(ctx, w, h);
+        this.addEventListener(this.gui);
+
+        var highPrecision = false;
+        var screenColorBuf = this.screenColorBuf = ctx.createTexture2D(null, w, h, { format: ctx.RGBA, type: highPrecision ? ctx.FLOAT : ctx.HALF_FLOAT });
+        var screenNormalBuf = this.screenNormalBuf = ctx.createTexture2D(null, w, h, { format: ctx.RGBA, type: highPrecision ? ctx.FLOAT : ctx.UNSIGNED_BYTE }); //RG32F
+        var screenDepthBuf = this.screenDepthBuf = ctx.createTexture2D(null, w, h, { magFilter: ctx.NEAREST, minFilter: ctx.NEAREST, format: ctx.DEPTH_COMPONENT, type: ctx.UNSIGNED_SHORT });
+        var screenFbo = this.screenFbo = ctx.createFramebuffer([
+            { texture: screenColorBuf },
+            { texture: screenNormalBuf }
+        ], { texture: screenDepthBuf })
+
+        this.camera  = new PerspCamera(45,this.getAspectRatio(), 1, 20.0);
         this.camera.lookAt([0, 1, 3], [0, 0, 0]);
         ctx.setProjectionMatrix(this.camera.getProjectionMatrix());
 
@@ -62,6 +80,41 @@ Window.create({
             { data: plane.positions, location: ctx.ATTRIB_POSITION },
             { data: plane.normals, location: ctx.ATTRIB_NORMAL }
         ], { data: plane.cells }, ctx.TRIANGLES);
+
+        this.entities = [];
+        for(var i=0; i<5; i++) {
+            var cubePosition = [i*1.5-3, 0, 0]
+            var cubeTransform = Mat4.createFromTranslation(cubePosition);
+            this.entities.push({
+                opacity: 1,
+                mesh: this.cubeMesh,
+                transform: cubeTransform
+            })
+
+            var teapotTransform = Mat4.createFromTranslation(cubePosition);
+            Mat4.translate(teapotTransform, [0,0.75, 0])
+            Mat4.rotate(teapotTransform,  Math.PI, [0, 1, 0])
+            this.entities.push({
+                opacity: 1,
+                mesh: this.teapotMesh,
+                transform: teapotTransform
+            })
+
+            var planeTransform = Mat4.createFromTranslation(cubePosition);
+            Mat4.translate(planeTransform, [0, 0, 0.51])
+            this.entities.push({
+                opacity: 0.5,
+                mesh: this.planeMesh,
+                transform: planeTransform
+            })
+        }
+
+        this.fx = createFx(ctx);
+
+        this.gui.addTexture2D('Screen color', screenColorBuf);
+        this.gui.addTexture2D('Screen normal', screenNormalBuf);
+        this.gui.addTexture2D('Screen depth', screenDepthBuf);
+
     },
     draw: function() {
         var ctx = this.getContext();
@@ -75,23 +128,36 @@ Window.create({
 
         ctx.bindProgram(this.showNormalsProgram);
 
-        for(var i=0; i<5; i++) {
+        var opaqueEntities = this.entities.filter(function(e) { return e.opacity == 1; })
+        var transparentEntities = this.entities.filter(function(e) { return e.opacity < 1; })
+
+        ctx.pushState(ctx.FRAMEBUFFER_BIT);
+        ctx.bindFramebuffer(this.screenFbo);
+
+        ctx.setClearColor(0, 0, 0, 0)
+        ctx.clear(ctx.COLOR_BIT | ctx.DEPTH_BIT)
+
+        opaqueEntities.forEach(function(entity) {
             ctx.pushModelMatrix();
-                ctx.translate([i*1.5-3, 0, 0])
-                ctx.bindMesh(this.cubeMesh);
+                ctx.setModelMatrix(entity.transform)
+                ctx.bindMesh(entity.mesh);
                 ctx.drawMesh();
-                ctx.pushModelMatrix();
-                    ctx.translate([0,0.75, 0])
-                    ctx.rotate(Math.PI, [0, 1, 0])
-                    ctx.bindMesh(this.teapotMesh);
-                    ctx.drawMesh();
-                ctx.popModelMatrix();
-                ctx.pushModelMatrix();
-                    ctx.translate([0,0, 0.51])
-                    ctx.bindMesh(this.planeMesh);
-                    ctx.drawMesh();
-                ctx.popModelMatrix();
-            ctx.popModelMatrix();
-        }
+            ctx.pushModelMatrix();
+        })
+
+        transparentEntities.forEach(function(entity) {
+            ctx.pushModelMatrix();
+                ctx.setModelMatrix(entity.transform)
+                ctx.bindMesh(entity.mesh);
+                ctx.drawMesh();
+            ctx.pushModelMatrix();
+        })
+
+        ctx.popState(ctx.FRAMEBUFFER_BIT);
+
+        var root = this.fx.reset();
+        root.image(this.screenColorBuf).blit({ width: this.getWidth(), height: this.getHeight() })
+
+        this.gui.draw();
     }
 })
