d3a43c44d157f6049c825e281a7502e85730aac6
Marcin Ignac <marcin.ignac@gmail.com>
2016-07-10 08:20:41 +0100

    spherical-harmonics Refactor mesh creation

--- spherical-harmonics/main.js
+++ spherical-harmonics/main.js
@@ -4,6 +4,7 @@ var Arcball = require('pex-cam/Arcball')
 var ShowColors = require('pex-materials/show-colors')
 var Vec3 = require('pex-math/Vec3')
 var computeNormals = require('normals').vertexNormals
+var createGrid = require('grid-cells')
 
 function factorial (n) {
   var result = 1
@@ -59,6 +60,71 @@ function SH (l, m, theta, phi) {
   }
 }
 
+function createSHMesh (ctx, l, m) {
+  var positions = []
+  var colors = []
+  var normals = []
+  var texCoords = []
+  var cells = []
+
+  var thetaN = 16 * 2
+  var phiN = 32 * 2
+  for (var thetaI = 0; thetaI < thetaN + 1; thetaI++) {
+    for (var phiI = 0; phiI < phiN + 1; phiI++) {
+      var s = thetaI / thetaN
+      var t = phiI / phiN
+      var theta = s * Math.PI
+      var phi = t * 2 * Math.PI
+      var x = Math.sin(theta) * Math.cos(phi)
+      var y = Math.sin(theta) * Math.sin(phi)
+      var z = Math.cos(theta)
+      var c = SH(l, m, theta, phi)
+      var scale = Math.abs(c)
+
+      positions.push([x * scale, y * scale, z * scale])
+
+      if (c > 0) {
+        colors.push([0, c, 0, 1])
+      } else {
+        colors.push([-c, 0, 0, 1])
+      }
+
+      texCoords.push([s, t])
+      normals.push([x, y, z])
+
+      if (thetaI < thetaN && phiI < phiN) {
+        cells.push([
+          thetaI * (phiN + 1) + phiI,
+          (thetaI + 1) * (phiN + 1) + phiI,
+          (thetaI + 1) * (phiN + 1) + phiI + 1
+        ])
+        cells.push([
+          thetaI * (phiN + 1) + phiI,
+          (thetaI + 1) * (phiN + 1) + phiI + 1,
+          thetaI * (phiN + 1) + phiI + 1
+        ])
+      }
+    }
+  }
+
+  normals = computeNormals(cells, positions)
+  for (var n = 0; n < positions.length; n++) {
+    var L = Vec3.normalize([1, 1, 1])
+    var N = normals[n]
+    var NdotL = Math.max(0, Vec3.dot(N, L))
+    NdotL = Math.pow(NdotL, 32)
+    Vec3.add(colors[n], Vec3.scale([0.2, 0.2, 0.2], NdotL))
+  }
+
+  var attributes = [
+    { data: positions, location: ctx.ATTRIB_POSITION },
+    { data: normals, location: ctx.ATTRIB_NORMAL },
+    { data: colors, location: ctx.ATTRIB_COLOR },
+    { data: texCoords, location: ctx.ATTRIB_TEX_COORD_0 }
+  ]
+  return ctx.createMesh(attributes, { data: cells }, ctx.TRIANGLES)
+}
+
 Window.create({
   settings: {
     width: 1280,
@@ -67,80 +133,29 @@ Window.create({
   },
   init: function () {
     var ctx = this.getContext()
+    var grid = createGrid(this.getWidth(), this.getHeight(), 9, 5, 0)
 
-    this.camera = new PerspCamera(45, this.getAspectRatio(), 0.001, 20.0)
+    this.camera = new PerspCamera(45, grid[0][2] / grid[0][3], 0.001, 20.0)
     this.camera.lookAt([0, 1, 3], [0, 0, 0])
     ctx.setProjectionMatrix(this.camera.getProjectionMatrix())
 
     this.arcball = new Arcball(this.camera, this.getWidth(), this.getHeight())
-    this.arcball.setDistance(3.0)
+    this.arcball.setDistance(2.0)
     this.addEventListener(this.arcball)
 
     this.showColorsProgram = ctx.createProgram(ShowColors.Vert, ShowColors.Frag)
     ctx.bindProgram(this.showColorsProgram)
 
-    var positions = []
-    var colors = []
-    var normals = []
-    var texCoords = []
-    var cells = []
+    this.meshes = []
 
-    var thetaN = 16 * 2
-    var phiN = 32 * 2
-    for (var thetaI = 0; thetaI < thetaN + 1; thetaI++) {
-      for (var phiI = 0; phiI < phiN + 1; phiI++) {
-        var s = thetaI / thetaN
-        var t = phiI / phiN
-        var theta = s * Math.PI
-        var phi = t * 2 * Math.PI
-        var x = Math.sin(theta) * Math.cos(phi)
-        var y = Math.sin(theta) * Math.sin(phi)
-        var z = Math.cos(theta)
-        var c = SH(3, -2, theta, phi)
-        var scale = Math.abs(c)
-
-        positions.push([x * scale, y * scale, z * scale])
-
-        if (c > 0) {
-          colors.push([0, c, 0, 1])
-        } else {
-          colors.push([-c, 0, 0, 1])
-        }
-
-        texCoords.push([s, t])
-        normals.push([x, y, z])
-
-        if (thetaI < thetaN && phiI < phiN) {
-          cells.push([
-            thetaI * (phiN + 1) + phiI,
-            (thetaI + 1) * (phiN + 1) + phiI,
-            (thetaI + 1) * (phiN + 1) + phiI + 1
-          ])
-          cells.push([
-            thetaI * (phiN + 1) + phiI,
-            (thetaI + 1) * (phiN + 1) + phiI + 1,
-            thetaI * (phiN + 1) + phiI + 1
-          ])
-        }
+    for (var l = 0; l <= 4; l++) {
+      for (var m = -l; m <= l; m++) {
+        this.meshes.push({
+          mesh: createSHMesh(ctx, l, m),
+          cell: grid[l * 9 + m + l]
+        })
       }
     }
-
-    normals = computeNormals(cells, positions)
-    for (var n = 0; n < positions.length; n++) {
-      var L = Vec3.normalize([1, 1, 1])
-      var N = normals[n]
-      var NdotL = Math.max(0, Vec3.dot(N, L))
-      NdotL = Math.pow(NdotL, 32)
-      Vec3.add(colors[n], Vec3.scale([0.2, 0.2, 0.2], NdotL))
-    }
-
-    var attributes = [
-      { data: positions, location: ctx.ATTRIB_POSITION },
-      { data: normals, location: ctx.ATTRIB_NORMAL },
-      { data: colors, location: ctx.ATTRIB_COLOR },
-      { data: texCoords, location: ctx.ATTRIB_TEX_COORD_0 }
-    ]
-    this.mesh = ctx.createMesh(attributes, { data: cells }, ctx.TRIANGLES)
   },
   draw: function () {
     var ctx = this.getContext()
@@ -152,8 +167,14 @@ Window.create({
     ctx.clear(ctx.COLOR_BIT | ctx.DEPTH_BIT)
     ctx.setDepthTest(true)
 
+    var H = this.getHeight()
+
     ctx.bindProgram(this.showColorsProgram)
-    ctx.bindMesh(this.mesh)
-    ctx.drawMesh()
+    this.meshes.forEach(function (meshSlot) {
+      var cell = meshSlot.cell
+      ctx.setViewport(cell[0], H - cell[1] - cell[3], cell[2], cell[3])
+      ctx.bindMesh(meshSlot.mesh)
+      ctx.drawMesh()
+    })
   }
 })
